1f60c601e7e50c11757d3e16f72c8b2ffe899fa2
Nathachanon <nathachanon.s@gmail.com>
2019-05-13 09:33:02 +0700

    แก้บัค ui

--- resources/views/buyer/searchResult.blade.php
+++ resources/views/buyer/searchResult.blade.php
@@ -17,6 +17,7 @@
     background-attachment: fixed;
   }
 </style>
+<link rel="stylesheet" type="text/css" href="layout/styles/shop_siderbar.css">
 <div class="shop">
   <div class="container">
     <div class="row ">
@@ -57,26 +58,8 @@
 
             </ul>
           </div>
-          <div class="sidebar_section filter_by_section">
-            <div class="sidebar_title">Filter By</div>
-            <div class="sidebar_subtitle">Price</div>
-            <div class="filter_price">
-              <div id="slider-range" class="slider_range"></div>
-              <p>Range: </p>
-              <p><input type="text" id="amount" class="amount" readonly style="border:0; font-weight:bold;"></p>
-            </div>
-          </div>
-          <div class="sidebar_section">
-            <div class="sidebar_subtitle color_subtitle">Color</div>
-            <ul class="colors_list">
-              <li class="color"><a href="#" style="background: #b19c83;"></a></li>
-              <li class="color"><a href="#" style="background: #000000;"></a></li>
-              <li class="color"><a href="#" style="background: #999999;"></a></li>
-              <li class="color"><a href="#" style="background: #0e8ce4;"></a></li>
-              <li class="color"><a href="#" style="background: #df3b3b;"></a></li>
-              <li class="color"><a href="#" style="background: #ffffff; border: solid 1px #e1e1e1;"></a></li>
-            </ul>
-          </div>
+         
+       
         </div>
       </div>
       <div class="col-lg-9 ">
